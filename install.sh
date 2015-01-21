804a6c6a664860a2e4edfb044865adda41f8d937
Jeremy Bradbury <jbradbury@isitedesign.com>
2015-01-20 20:04:11 -0800

    we don't need this

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ e_warn='\x1b[33;01m[warning]:\x1b[0m'           # yellow
 e_success='\x1b[32;01m[success]:\x1b[0m'        # green
 e_info='\x1b[30;01m[inform]:\x1b[0m'            # black
 e_input='\x1b[34;01m[input required]:\x1b[0m'   # blue
-e_line='-------------------------------------------------------------------';
 s_title='Development Environment Provisioning for Mac OSX 10.10 (Yosemite)';
 ### /constants ###
 
